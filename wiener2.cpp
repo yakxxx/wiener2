12c895bff08b8a146bba0fe7276501788cf25ccd
yak <yak@rincewind>
2012-05-30 14:44:59 +0200

    refact.

--- wiener2.cpp
+++ wiener2.cpp
@@ -3,10 +3,14 @@
 #include "opencv2/highgui/highgui.hpp"
 
 using namespace cv;
-Mat padd_image(Mat I);
-Mat with_noise(Mat image, int stddev);
-Mat get_spectrum(Mat I);
 Mat wiener2(Mat I, Mat image_spectrum, int noise_stddev);
+Mat padd_image(Mat I);
+
+Mat get_spectrum(Mat I);
+Mat get_dft(Mat I);
+
+Mat with_noise(Mat image, int stddev);
+Mat rand_noise(Mat I, int stddev);
 
 
 
@@ -18,52 +22,19 @@ int main(void) {
 	Mat spectrum = get_spectrum(padded);
 	Mat enhanced = wiener2(noisy, spectrum, 50);
 
-//	Mat spectrum = get_spectrum(I);
-//
-//	spectrum += Scalar::all(1);                    // switch to logarithmic scale
-//	log(spectrum, spectrum);
-//	normalize(spectrum, spectrum, 0, 1, CV_MINMAX);
-//
 	imshow("image 1", I);
 	imshow("image 2", noisy);
 	imshow("image 3", enhanced);
 	waitKey();
 }
 
-
-Mat get_spectrum(Mat I){
-	Mat padded = padd_image(I);
-	Mat padded_f;
-	padded.convertTo(padded_f, CV_32F);
-	Mat planes[] = {Mat(padded_f.rows, padded_f.cols, CV_32FC1), Mat(padded_f.rows, padded_f.cols, CV_32FC1)};
-	Mat complexI;
-	dft(padded_f, complexI, DFT_COMPLEX_OUTPUT);
-
-	split(complexI, planes);                   // planes[0] = Re(DFT(I), planes[1] = Im(DFT(I))
-	magnitude(planes[0], planes[1], planes[0]);// planes[0] = magnitude
-	Mat magI = planes[0];
-	return magI;
-}
-
-Mat with_noise(Mat image, int stddev){
-	Mat noise(image.rows, image.cols, CV_8U);
-	randn(noise,Scalar::all(0), Scalar::all(stddev));
-	Mat noisy = image.clone();
-	noisy += noise;
-	return noisy;
-}
-
 Mat wiener2(Mat I, Mat image_spectrum, int noise_stddev){
 	Mat padded = padd_image(I);
-	Mat noise = Mat::zeros(I.rows, I.cols, CV_32F);
-	randn(noise,Scalar::all(0), Scalar::all(noise_stddev));
+	Mat noise = rand_noise(padded, noise_stddev);
 	Mat noise_spectrum = get_spectrum(noise);
 
-	Mat planes[] = {Mat_<float>(padded), Mat::zeros(padded.size(), CV_32F)};
-	Mat complexI;
-	merge(planes, 2, complexI);
-
-	dft(complexI, complexI);
+	Mat planes[2];
+	Mat complexI = get_dft(padded);
 	split(complexI, planes);	// planes[0] = Re(DFT(I), planes[1] = Im(DFT(I))
 
 	Mat image_spectrum_2 = image_spectrum * image_spectrum;
@@ -86,6 +57,39 @@ Mat padd_image(Mat I){
 	int m = getOptimalDFTSize( I.rows );
 	int n = getOptimalDFTSize( I.cols ); // on the border add zero pixels
 	copyMakeBorder(I, padded, 0, m - I.rows, 0, n - I.cols, BORDER_CONSTANT, Scalar::all(0));
-
 	return padded;
 }
+
+Mat get_spectrum(Mat I){
+	Mat complexI = get_dft(I);
+	Mat planes[2];
+	split(complexI, planes);                   // planes[0] = Re(DFT(I), planes[1] = Im(DFT(I))
+	magnitude(planes[0], planes[1], planes[0]);// planes[0] = magnitude
+	Mat magI = planes[0];
+	return magI;
+}
+
+Mat get_dft(Mat I){
+	Mat image;
+	I.convertTo(image, CV_32F);
+	Mat planes[] = {Mat_<float>(image), Mat::zeros(image.size(), CV_32F)};
+	Mat complexI;
+	merge(planes, 2, complexI);
+	dft(complexI, complexI);
+	return complexI;
+}
+
+Mat with_noise(Mat image, int stddev){
+	Mat noise(image.rows, image.cols, CV_8U);
+	rand_noise(image, stddev).convertTo(noise, CV_8U);
+	Mat noisy = image.clone();
+	noisy += noise;
+	return noisy;
+}
+
+Mat rand_noise(Mat I, int stddev){
+	Mat noise = Mat::zeros(I.rows, I.cols, CV_32F);
+	randn(noise,Scalar::all(0), Scalar::all(stddev));
+	return noise;
+}
+
